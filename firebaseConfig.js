053ae4427be66f5d91f9f6859a768b0858ba8974
Thanhnghia440 <thanhnghia20062009@gmail.com>
2026-04-12 16:56:31 +0700

    testjsi

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCBh-wCR02v7WDHbFyiOdbauRi0pUUZ25Q",
+  authDomain: "finaljsi-7d280.firebaseapp.com",
+  projectId: "finaljsi-7d280",
+  storageBucket: "finaljsi-7d280.firebasestorage.app",
+  messagingSenderId: "265792097124",
+  appId: "1:265792097124:web:af88c733fa777fac7b4871",
+  measurementId: "G-EJBWKYC7JQ"
+};
